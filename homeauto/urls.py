b1633977d9c88becbc8b7f7c1b7b71e5f2363a05
Adewole2016 <kunleadewole2008@gmail.com>
2025-08-27 13:08:36 +0100

    Update urls.py

--- homeauto/urls.py
+++ homeauto/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('api/device/<str:device_id>/desired/', views.api_desired_state, name='api_desired_state'),
     path('api/device/<str:device_id>/report/', views.api_report_state, name='api_report_state'),
     path('device/<str:device_id>/toggle/<int:channel>/', views.toggle_channel, name='toggle_channel'),
-    path('device/<str:device_id>/all/<str:action>/', views.all_channels, name='all_channels'),
+    path('device/<str:device_id>/all/<str:action>/', views.all_channels, name='all_channels'),\
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
+
