121a12a988ca3be68001a0392e478428634c0c02
Adewole2016 <kunleadewole2008@gmail.com>
2025-08-29 11:46:06 +0100

    Update views.py

--- automation/views.py
+++ automation/views.py
@@ -25,7 +25,7 @@ def dashboard(request):
     device, _ = Device.objects.get_or_create(
         device_id='NODEMCU-01', defaults={'name': 'Home Devices Controller'}
     )
-    reports = device.reports.all().order_by('-created_at')[:5]
+    reports = device.reports.all().order_by('-created_at')[:4]
     
     return render(request, 'dashboard.html', {
         'device': device,
